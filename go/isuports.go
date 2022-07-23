a68cb3330cd65bcc04eda6c2d1ccf2f6d166fe38
michi-nakaya <michi.nakaya@gmail.com>
2022-07-23 15:37:08 +0900

    fix playersAddHandler

--- go/isuports.go
+++ go/isuports.go
@@ -809,34 +809,34 @@ func playersAddHandler(c echo.Context) error {
 
 	pds := make([]PlayerDetail, 0, len(displayNames))
 
-	// TODO: bulk insert
-	// retrievePlayer は不要
+	var ps []PlayerRow
+	now := time.Now().Unix()
 	for _, displayName := range displayNames {
 		id, err := dispenseID(ctx)
 		if err != nil {
 			return fmt.Errorf("error dispenseID: %w", err)
 		}
 
-		now := time.Now().Unix()
-		if _, err := tenantDB.ExecContext(
-			ctx,
-			"INSERT INTO player (id, tenant_id, display_name, is_disqualified, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
-			id, v.tenantID, displayName, false, now, now,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player at tenantDB: id=%s, displayName=%s, isDisqualified=%t, createdAt=%d, updatedAt=%d, %w",
-				id, displayName, false, now, now, err,
-			)
-		}
-		p, err := retrievePlayer(ctx, tenantDB, v.tenantID, id)
-		if err != nil {
-			return fmt.Errorf("error retrievePlayer: %w", err)
-		}
-		pds = append(pds, PlayerDetail{
-			ID:             p.ID,
-			DisplayName:    p.DisplayName,
-			IsDisqualified: p.IsDisqualified,
+		ps = append(ps, PlayerRow{
+			TenantID: v.tenantID,
+			ID: id,
+			DisplayName: displayName,
+			IsDisqualified: false,
+			CreatedAt: now,
+			UpdatedAt: now,
 		})
+		pds = append(pds, PlayerDetail{
+			ID: id,
+			DisplayName: displayName,
+			IsDisqualified: false,
+		})
+	}
+	if _, err := tenantDB.NamedExecContext(
+		ctx,
+		"INSERT INTO player (id, tenant_id, display_name, is_disqualified, created_at, updated_at) VALUES (:id, :tenant_id, :display_name, :is_disqualified, :created_at, :updated_at)",
+		ps,
+	); err != nil {
+		return fmt.Errorf("failed to query player: %w", err)
 	}
 
 	res := PlayersAddHandlerResult{
