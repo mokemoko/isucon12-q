0e5f9fd20a28b907c90d9950670b0d985a985ce5
Tomohiko Nakamura <nakamura_t@r.recruit.co.jp>
2022-07-23 16:11:04 +0900

    fix

--- go/isuports.go
+++ go/isuports.go
@@ -1365,7 +1365,7 @@ func competitionRankingHandler(c echo.Context) error {
 	if err := tenantDB.SelectContext(
 		ctx,
 		&pss,
-		"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY score DESC, row_num ASC OFFSET ? LIMIT 100",
+		"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY score DESC, row_num ASC LIMIT 100 OFFSET ?",
 		tenant.ID,
 		competitionID,
 		rankAfter,
